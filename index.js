213cb0393cbe6417a59c27a52e2c5e962135975b
GCdM <gabrielcm.br@gmail.com>
2018-07-14 17:58:10 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'anyone'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'
+}
